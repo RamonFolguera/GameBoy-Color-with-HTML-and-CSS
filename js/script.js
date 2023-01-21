70792c62a5482c5c90a488da54291398dd31a3a7
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-01-22 00:10:51 +0100

    JavaScript añadido, función de encendido y video de inicio añadido

---
+++ js/script.js
@@ -0,0 +1,45 @@
+/* 
+
+Tareas para hacer la consola interactiva:
+
+1. Accionar botones al clickar encima.
+2. Encender la pantalla al apretar START y que aparezca la pantalla de inicio:
+
+<video width="160" height="154" controls src="./video/Game Boy Advance SP Intro HD (60fps).mp4" type="video/mp4"></video>
+
+Funciones:
+
+- startConsole()
+
+*/
+
+
+// Coger elementos que necesitamos del DOM y almacenarlos en variables:
+
+
+
+const displayContainer = document.querySelector('.display-container');
+const startBtn= document.querySelector('#start-button');
+
+//Creamos el <video> element
+const videoElement = document.createElement('video');
+videoElement.setAttribute('width','160');
+videoElement.setAttribute('height','154');
+videoElement.setAttribute('src','./video/Game Boy Advance SP Intro HD (60fps).mp4');
+videoElement.setAttribute('type','video/mp4');
+console.log(displayContainer)
+
+//añadimos el video al div: display-container
+
+
+
+
+//Encendido del GameBoy
+
+const startConsole = () => {
+    const videoDiv = displayContainer.appendChild(videoElement);
+    displayContainer.style.background = "#bebebe";
+    videoElement.play()    
+};
+
+console.log('hello, como estas')
